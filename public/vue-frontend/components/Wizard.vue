177bdc807e0538e88667d3cf4232c5137bfca6e4
Lukasz Wojcik <arbor@10g.pl>
2021-12-12 12:58:47 +0100

    dsa
    dsa

---
+++ public/vue-frontend/components/Wizard.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="p-8 mx-auto my-8 w-1/2 min-h-screen shadow-md hover:shadow-xl bg-white dark:bg-black">
+    <span class="text-xl text-black dark:text-white"> hallo </span>
+  </div>
+</template>
+
+<script lang="ts"></script>
